b5dc607459df09b37d5ec3f2b1235898b5e66bde
artemiyjjj <artemiyromanoff@gmail.com>
2024-01-13 20:21:56 +0300

    Test fixes and unit test for isa module

--- test/integration_test.py
+++ test/integration_test.py
@@ -1,3 +1,12 @@
+import contextlib
+import io
+import logging
+import os
+import sys
+import tempfile
+
+import pytest
+
 
 @pytest.mark.golden_test("golden_tests/integration/*.yml")
 def test_translator_and_machine(golden, caplog) -> void:
