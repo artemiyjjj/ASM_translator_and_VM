905350c3c597d2510e06850aa220c5a7225c4e31
artemiyjjj <artemiyromanoff@gmail.com>
2024-02-24 17:18:33 +0300

    Add io and interruption support

--- src/devices.py
+++ src/devices.py
@@ -1,103 +1,92 @@
-from abc import ABC, abstractmethod
-import BitVector
+from __future__ import annotations
 
-def get_spi_shift_reg_length() -> int:
-    """ Количество битов в сдвиговых регистрах устройств SPI."""
-    return 8
 
-class SPISlave(ABC):
-    """ Абстрактный класс для объявления методов SPI-slave'а
-
-    Объявляет методы, необходимые к реализации имплементирующим классом,
-     и определяет методы для использования SPI-устройства.
+class IODevice:
+    """ Сигналы и интерфейс для использования SPI-устройства.
 
     CS сигнал по умолчанию High. Low означает, что устройство в данный момент выбрано к обмену.
-    Если устройство не выбрано, то оно должно игнорировать сигналы SCLK и MOSI, а также отключить
+    Если устройство не выбрано, то оно должно игнорировать сигналы SCLK и MOSI (т.к. считается выключенным), а также отключить
     от шины сигнал MISO (подавать сигнал с высоким импедансом).
+
+    Сигналы реализованы переменными, т.к. в рамках устройства сохраняют состояние до смены состояния
     """
-    _SCLK: bool | None = False
-    _CS: bool = True
-    _MOSI: bool | None = None
-    _MISO: bool | None = None
+    _interruption_request: bool
+    _transfer_finished: bool
+    _sclk: bool | None
+    _cs: bool
+    _mosi: bool | None
+    _miso: bool | None
 
-    _timer: int = 0
-
-    _shift_register: BitVector = BitVector(size = get_spi_shift_reg_length())
-
-    # def cycle():
-
-    def set_SCLK(self, SCLK_signal: bool) -> bool:
-        self._SCLK = SCLK_signal if self._CS == False else None
-
-    def set_CS(self, CS_signal: bool) -> bool:
-        self._CS = CS_signal
-        print("MOSI MISO before ", self._MOSI, " ", self._MISO)
-        self._MOSI, self._MISO = None if CS_signal == True else self._MOSI, self._MISO
-        print("MOSI MISO after ", self._MOSI, " ", self._MISO)
-
-    def set_MISO(self, MISO_signal: bool) -> bool:
-        self._MISO = MISO_signal if self._CS == False else None
-
-    def set_MOSI(self, MOSI_signal: bool) -> bool:
-        self._MOSI = MOSI_signal if self._CS == False else None
-
-class SPIMaster(ABC):
-    """ Абстрактный класс для объявления методов SPI-master'а
-
-    Объявляет методы, необходимые к реализации имплементирующим классом,
-     и определяет методы для использования SPI-устройства.
-    """
-    # Устройства, которые могут быть выбраны с помощью Chip Select
-    _devices: list[SPISlave] | None = None
-
-    # Счётчик смен сигнала SCLK (переданных бит)
-    _timer: int = 0
-
-    # Сдвиговый регистр, последовательно передающий биты в MOSI
-    _shift_register: BitVector = BitVector(size = get_spi_shift_reg_length())
-
+    _shift_register: int
+    _step_counter: int
 
     def __init__(self) -> None:
-        print("SPI MASTER INIT")
+        self._shift_register = 0
+        self._step_counter = 0
+        self._interruption_request = False
+        self._transfer_finished = False
+        self._sclk = None
+        self._cs: bool = True
+        self._mosi = None
+        self._miso = None
 
-    def send_signals(self, chip_index: int, bit: bool) -> None:
-        self.SCLK()
-        self.CS(chip_index)
-        self.MOSI(bit)
-        self.MISO()
-        _timer += 1
+    def signal_set_sclk(self, sclk_signal: bool) -> None:
+        """ Передача сигнала SCLK, начало обмена, если сигнал поменялся на "1"."""
+        if self._cs is False and self._sclk is not sclk_signal and sclk_signal is True:
+            self._step_counter += 1
+            if self._step_counter == 8:
+                self._transfer_finished = True
+            self._sclk = sclk_signal
+            # Обмен данными по фронту
+            assert self._mosi is not None
+            assert self._miso is not None
+            self._shift_register <<= 1
+            self._miso = True if self._shift_register & 256 == 256 else False
+            self._shift_register &= 255
+            self._shift_register |= self._mosi
 
-    def SCLK(self) -> None:
-        for device in self.devices:
-            device.set_SCLK
+    def signal_set_cs(self, cs_signal: bool) -> None:
+        self._cs = cs_signal
+        # print("MOSI MISO before ", self._MOSI, " ", self._MISO)
+        if cs_signal is True:
+            self._mosi = self.miso = self._sclk = None
+        else:
+            self._mosi = self.miso = self._sclk = False
+            # self._shift_register = 0
+        # print("MOSI MISO after ", self._MOSI, " ", self._MISO)
 
-    def CS(self, chip_index: int) -> None:
-        for device_index, device in enumerate(self.devices):
-            signal: bool = False if device_index == chip_index else True
-            device.set_CS(signal)
-
-    def MOSI(self, bit: bool) -> None:
-        pass
-
-    def MISO(self) -> None:
-        pass
-
+    def signal_set_mosi(self, mosi_signal: bool) -> None:
+        if self._cs is False:
+            self._MOSI = mosi_signal
 
 
-
-class IODevice(SPISlave):
-    data_reg
-    interruption_request:
-
-
-class SPIController(SPIMaster):
+class SPIController:
     """ Контроллер управления передачей данных между машиной и переферийными устройствами
     
     """
+    # Устройства, которые могут быть выбраны с помощью Chip Select
+    _devices: dict[int, IODevice]
 
-    def __init__(self) -> None:
-        super().__init__()
+    # Сдвиговый регистр, последовательно передающий биты в MOSI
+    _shift_register: int
 
-    def __init__(self, devices: list[IODevice]) -> None:
+    def __init__(self, slaves: dict[int, IODevice] = dict()) -> None:
+        self._devices = slaves
+        _shift_register = 0
 
+    def signal_cs(self, chip_index: int, signal: bool) -> None:
+        """ Демультиплексор для выбора устройства, на которое подать сигнал CS.
 
+        Уровень сигнала поддерживается до смены."""
+        if self._devices.get(chip_index) is not None:
+            self._devices[chip_index].signal_set_cs(cs_signal = signal)
+
+    def signal_sclk(self, signal: bool) -> None:
+        """ Передача сигнала SCLK на линию SCLK (ко всем устройствам)."""
+        for device in self._devices.values():
+            device.signal_set_sclk(sclk_signal = signal)
+
+    def signal_mosi(self, signal: bool) -> None:
+        """ Передача сигнала MOSI на линию MOSI (ко всем устройствам)."""
+        for device in self._devices.values():
+            device.signal_set_mosi(mosi_signal = signal)
--- src/machine.py
+++ src/machine.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 import logging
 import sys
 
-# from devices import IODevice, SPIController
+from devices import IODevice, SPIController
 from isa import Code, DataTerm, Mode, Opcode, StatementTerm, read_code
 
 # Начальное состояние регистра - счётчика команд
@@ -22,6 +22,9 @@ def get_interruption_vector_length() -> int:
 
 def raise_error(err_msg: str = "") -> None:
     raise ValueError("Internal error X_X : " + err_msg)
+
+def bool2int(val: bool) -> int:
+    return 1 if val else 0
 
 
 class DataPath:
@@ -173,7 +176,7 @@ class DataPath:
                 case 2:
                     self._output_buffer_register = self._left_register * self._right_register
                 case 3:
-                    self._output_buffer_register = self._left_register / self._right_register
+                    self._output_buffer_register = self._left_register // self._right_register
                 case 4:
                     self._output_buffer_register = self._left_register % self._right_register
                 case 5:
@@ -194,7 +197,8 @@ class DataPath:
         self._neg_flag = self._alu._res_neg
 
     def _read_memory(self) -> int:
-        """Чтение из памяти по адресу равному значению адресного регистра значения в аккумулятор."""
+        """Чтение из памяти по адресу из адресного регистра значения в аккумулятор."""
+        assert isinstance(self._memory[self._address_register].value, int)
         return self._memory[self._address_register].value
 
     def _write_memory(self) -> None:
@@ -236,10 +240,13 @@ class ControlUnit:
     _memory: list[StatementTerm | DataTerm]
     """ Общая память, из которой модулем управления выбираются команды."""
 
+    _spi_controller: SPIController
+    """ Контроллер ввода/вывода интерфейса SPI."""
+
     _data_path: DataPath
     """ Соединение с DataPath для управления манипулированием данными."""
 
-    def __init__(self, common_memory: list[StatementTerm | DataTerm], data_path: DataPath) -> None:
+    def __init__(self, common_memory: list[StatementTerm | DataTerm], data_path: DataPath, spi_controller: SPIController) -> None:
         self._tick = 0
         self._interruption_enabled = False
         self._interruption_request = False
@@ -249,6 +256,7 @@ class ControlUnit:
         self._instruction_decoder = ControlUnit.InstructionDecoder()
         self._memory = common_memory
         self._data_path = data_path
+        self._spi_controller = spi_controller
 
     class InstructionDecoder:
         _step_counter: int
@@ -280,10 +288,66 @@ class ControlUnit:
 
         # ???
 
-    def signal_output(self) -> None:
-        output_symbol = chr(self._accumulator_register)
-        logging.info("Output: %s + %s", repr("".join(self.output_buffer)), repr(output_symbol))
-        self._output_buffer.append[output_symbol]
+    def signal_input(self, select: int) -> None:
+        """ Сигнал ввода данных в аккумулятор с указанного порта ввода.
+
+        Выбор порта происходит постредством передачи управляющих сигналов на демультиплексор контроллера SPI.
+        В реализации процессора доступно 8 устройств ввода/вывода.
+        Управляющие сигналы:
+        0 - SCLK
+        1 - MOSI
+        2 - MISO
+        3 - CS0
+        4 - CS1
+        ...
+        10 - CS7
+        11 - INT0
+        12 - INT1
+        ...
+        17 - INT7
+        """
+        signal: bool | None = None
+        match select:
+            case 2:
+                signal = self._spi_controller._devices.get(2)._miso
+            case 11:
+                signal = self._spi_controller._devices.get(11)
+            case 1 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10:
+                pass
+        if signal is not None:
+                self._data_path._accumulator_register | signal
+
+    def signal_output(self, select: int) -> None:
+        """ Сигнал вывода данных из аккумулятора на указанный порт вывода.
+
+        Выбор порта происходит постредством передачи управляющих сигналов на демультиплексор контроллера SPI.
+        В реализации процессора доступно 8 устройств ввода/вывода.
+        Управляющие сигналы:
+        0 - SCLK
+        1 - MOSI
+        2 - MISO
+        3 - CS0
+        4 - CS1
+        ...
+        10 - CS7
+        11 - INT0
+        ...
+        17 - INT7
+        """
+        signal: bool = True if self._data_path._accumulator_register & 1 == 1 else False
+        match select:
+            case 0:
+                self._spi_controller.signal_sclk(signal = signal)
+            case 1:
+                self._spi_controller.signal_mosi(signal = signal)
+            case 2:
+                pass
+            case 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10:
+                self._spi_controller.signal_cs(chip_select = select, signal = signal)
+            case 11:
+                self._spi_controller._interruption_request = signal
+            case _:
+                raise_error("Incorrect control_unit/signal_spi select")
 
     def signal_latch_address_register(self, select: int) -> None:
         """Сигнал записи данных в адресный регистр через мультиплексор.
@@ -297,6 +361,7 @@ class ControlUnit:
         """
         match select:
             case 0:
+                assert self._instruction_register is not None
                 self._data_path._address_register = self._instruction_register.arg
             case 1:
                 self._data_path._address_register = self._data_path._buffer_register
@@ -336,13 +401,21 @@ class ControlUnit:
         Селекторы:
         0 - instr_arg (control_unit/instruction_register.arg)
         1 - mem (data_path/memory)
-        2 - ...
+        2 - io_int (SPISlaves/_interruption_request demultiplexer)
         """
         match select:
             case 0:
+                assert self._instruction_register is not None
                 self._data_path._buffer_register = self._instruction_register.arg
             case 1:
                 self._data_path._buffer_register = self._data_path._read_memory()
+            # Вызывается только в цикле проверки прерываний
+            case 2:
+                device_index: int
+                for index, device in self._spi_controller._devices.items():
+                    if device._interruption_request:
+                        device_index = index
+                self._data_path._buffer_register = device_index
             case _:
                 raise_error("Incorrect control_unit/signal_buff select")
 
@@ -399,7 +472,7 @@ class ControlUnit:
         self._data_path._alu.inc(select[2])
         self._data_path._alu.dec(select[3])
         self._data_path._alu.operation(select[4])
-        self._data_path._alu.update_flags()
+        self._data_path.update_flags()
         self._data_path._alu.reset_registers()
 
     def signal_latch_programm_counter_register(self, select: int) -> None:
@@ -419,6 +492,38 @@ class ControlUnit:
             case _:
                 raise_error("Incorrect control_unit/signal_pc select")
 
+
+    def _prepare_for_interruption(self) -> None:
+        """ Подготовка машины к обработке прерывания.
+
+        Сохранение аккумулятора и счётчика команд в определенные для этого ячейки памяти,
+        установка флага обработки прерывания и сохранение номера прерывания в аккумулятор.
+        """
+        # Запись в память значения аккумулятора
+        self.signal_latch_address_register(select=2)
+        self.perform_tick()
+        self._data_path._write_memory()
+        self.perform_tick()
+        # Запись в память значения регистра - счётчика команд
+        self.signal_latch_accumulator_register(select=3)
+        self.perform_tick()
+        self.signal_latch_address_register(select=3)
+        self.perform_tick()
+        self._data_path._write_memory()
+        self.perform_tick()
+        # Изменение значения счётчика команд
+        self.signal_latch_address_register(select=1)
+        self.perform_tick()
+        self.signal_latch_buffer_register(select=1)
+        self.perform_tick()
+        self.signal_latch_programm_counter_register(select=0)
+        self.perform_tick()
+        self._interruption_state = True
+        self.signal_latch_arithmetical_logical_unit(select=[0, 3, 3, 3, 6])
+        self.signal_latch_accumulator_register(select=1)
+        self.perform_tick()
+
+
     def _select_instruction(self) -> None:
         """Цикл выборки инструкции из памяти по адресу счётчика команд.
 
@@ -429,6 +534,7 @@ class ControlUnit:
         self.perform_tick()
 
     def _decode_instruction(self) -> None:
+        assert self._instruction_register is not None
         self._instruction_decoder.signal_latch_opcode(self._instruction_register.opcode)
         self._instruction_decoder.signal_latch_mode(self._instruction_register.mode)
         print("decoder: ", self._instruction_decoder._opcode, self._instruction_decoder._mode)
@@ -460,11 +566,7 @@ class ControlUnit:
             case None:
                 self.perform_tick()
             case _:
-                raise ValueError(
-                    "Mode at instruction on line {} in source code is incorrect.".format(
-                        self._instruction_register.line
-                    )
-                )
+                raise ValueError("Mode at some instruction in source code is incorrect.")
 
     def _execute_instruction(self) -> None:
         match self._instruction_decoder._opcode:
@@ -478,11 +580,11 @@ class ControlUnit:
                 self._data_path._write_memory()
                 self.perform_tick()
             case Opcode.IN:
-                pass
-
+                self.signal_input(select = self._data_path._buffer_register)
+                self.perform_tick()
             case Opcode.OUT:
-                pass
-
+                self.signal_output(select = self._data_path._buffer_register)
+                self.perform_tick()
             case Opcode.ADD:
                 self.signal_latch_arithmetical_logical_unit(select=[2, 3, 3, 3, 0])
                 self.signal_latch_accumulator_register(select=1)
@@ -554,25 +656,7 @@ class ControlUnit:
                     self.signal_latch_programm_counter_register(select=0)
                 self.perform_tick()
             case Opcode.INT:
-                # Запись в память значения аккумулятора
-                self.signal_latch_address_register(select=2)
-                self.perform_tick()
-                self._data_path._write_memory()
-                self.perform_tick()
-                # Запись в память значения регистра - счётчика команд
-                self.signal_latch_accumulator_register(select=3)
-                self.perform_tick()
-                self.signal_latch_address_register(select=3)
-                self.perform_tick()
-                self._data_path._write_memory()
-                self.perform_tick()
-                # Изменение значения счётчика команд
-                self.signal_latch_address_register(select=1)
-                self.perform_tick()
-                self.signal_latch_buffer_register(select=1)
-                self.perform_tick()
-                self.signal_latch_programm_counter_register(select=0)
-                self.perform_tick()
+                self._prepare_for_interruption()
             case Opcode.FI:
                 # Чтение из памяти значение счётчика команд
                 self.signal_latch_address_register(select=3)
@@ -586,6 +670,8 @@ class ControlUnit:
                 self.perform_tick()
                 self.signal_latch_accumulator_register(select=0)
                 self.perform_tick()
+                # Обнуление флага обработки прерывания
+                self._interruption_state = False
             case Opcode.ENI:
                 self._interruption_enabled = True
                 self.perform_tick()
@@ -601,9 +687,8 @@ class ControlUnit:
 
     def _check_interruption(self) -> None:
         if self._interruption_enabled and self._interruption_request:
-            pass
-            # вызов программного обработчика прерываний для опроса slaves о прерывании
-        pass
+            self.signal_latch_buffer_register(select=2)
+            self._prepare_for_interruption()
 
     def execute_next_command(self) -> None:
         """Выполнение циклов исполнения команды."""
@@ -626,7 +711,7 @@ class Machine:
     _common_memory: list[StatementTerm | DataTerm]
 
     # Список "подключённых" устройств ввода/вывода
-    # io_devices: list[IODevice] = []
+    _io_devices: dict[int, IODevice]
 
     # Модель тракта данных в машине.
     _data_path: DataPath
@@ -634,25 +719,28 @@ class Machine:
     # Модель управляющего модуля в машине.
     _control_unit: ControlUnit
 
-    # Буфер для хранения расписания ввода символов
-    _input_buffer: list[tuple[int, str]]
+    # Контроллер ввода/вывода интерфейса SPI.
+    _spi_controller: SPIController
 
     _output_buffer: list[str]
 
-    def __init__(self, memory_size: int = 4096, input_buffer: list[tuple[int, str]] = []):
+    def __init__(self,
+                memory_size: int = 4096,
+                io_devices: dict[int, IODevice] = dict()
+                ) -> None:
         assert memory_size > 0, "Memory size should not be zero."
         self._memory_size = memory_size
         # Возможность для использования стека, если не задавать размер памяти, равный количеству машинных выражений
         self._common_memory = [DataTerm(index = index) for index in range(0, memory_size)]
+        self._io_devices = io_devices
         self._data_path = DataPath(self._common_memory)
-        self._control_unit = ControlUnit(common_memory = self._common_memory, data_path = self._data_path)
-        self._input_buffer = input_buffer
+        self._spi_controller = SPIController(slaves = self._io_devices)
+        self._control_unit = ControlUnit(common_memory = self._common_memory, data_path = self._data_path, spi_controller = self._spi_controller)
         self._output_buffer = []
 
     def __repr__(self) -> str:
         """Вернуть строковое представление состояния процессора."""
-        def bool2int(val: bool) -> int:
-            return 1 if val else 0
+        assert self._control_unit._instruction_register is not None
         return "TICK: {:3} PC: {:3} IR: '{:^11}' IRQ: {} IE: {} IS: {} AC: {:^10} AR: {:3} BR: {:3} N: {}, Z: {}".format(
             self._control_unit.get_tick(),
             self._control_unit._programm_counter_register,
@@ -663,17 +751,16 @@ class Machine:
             self._control_unit._data_path._accumulator_register,
             self._control_unit._data_path._address_register,
             self._control_unit._data_path._buffer_register,
-            bool2int(self._control_unit._data_path.negative),
-            bool2int(self._control_unit._data_path.zero)
+            bool2int(self._control_unit._data_path.negative()),
+            bool2int(self._control_unit._data_path.zero())
         )
+
 
     @staticmethod
     def parse_schedule(list_tuple_text: str) -> list[tuple[int, str]]:
         """Парсинг расписания ввода символов по тактам из текста с соответствующими данными."""
-        list_schedule: list[tuple[int, str]] = []
-        for t in list_tuple_text:
-            a, b = t.strip("()").split(",")
-            list_schedule.append((int(a), int(b)))
+        # мб убрать кавычки
+        list_schedule: list[tuple[int, str]] = list(eval(list_tuple_text)) if list_tuple_text.strip() != "" else []
         return list_schedule
 
     def simulation(
@@ -685,9 +772,19 @@ class Machine:
         """
         assert limit > 0, "Simulation failed: Limit can not be negative or zero."
         self._common_memory[:len(code.contents)] = code.contents
+        cur_schedule: int | None = 0 if len(input_schedule) > 0 else None
         try:
             while self._control_unit.get_tick() < limit:
+                # Логика управлением расписания вводом/выводом
+                if cur_schedule is not None and self._control_unit.get_tick() >= input_schedule[cur_schedule][0]:
+                    self._spi_controller._devices[3]._shift_register = input_schedule[cur_schedule][1]
+                    self._spi_controller._devices[3]._interruption_request = True
+
                 self._control_unit.execute_next_command()
+                # Сбор данных с устройств вывода
+                if self._spi_controller._devices[4]._transfer_finished:
+                    self._output_buffer.append(chr(self._spi_controller._devices[4]._shift_register))
+                    self._spi_controller._devices[4]._transfer_finished = False
                 logging.info(self.__repr__())
         except StopIteration:
             logging.info(self.__repr__())
@@ -724,7 +821,10 @@ def main(code: Code, input_file_name: str) -> None:
         logging.error(e)
         return
 
-    machine = Machine(memory_size = len(code.contents))
+    machine = Machine(
+        memory_size = len(code.contents),
+        io_devices = {index: IODevice() for index in [0, 1, 2, 3, 4, 5, 6]}
+        )
 
     try:
         output, instr_counter, ticks = machine.simulation(
